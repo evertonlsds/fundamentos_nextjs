a8270466d7f96677ca2b305f39b2e513c9fd04c2
Everton santos <everton0102@gmail.com>
2021-12-25 18:47:13 -0300

    rotas

--- src/pages/questao.jsx
+++ src/pages/questao.jsx
@@ -1,10 +1,32 @@
+import { useEffect, useState } from "react"
+
 export default function questao(){
-    fetch('http://localhost:3000/api/questao/3333')
-        .then(resp => resp.json())
-        .then(json => console.log(json))
+    const[questao, setQuestao]= useState(null)
+
+    useEffect(() => {
+        fetch('http://localhost:3000/api/questao/3333')
+            .then(resp => resp.json())
+            .then(setQuestao)
+    },[])
+    
+    function renderizarRespostas(){
+        if(questao){
+            return questao.respostas.map(resp => {
+                return <li>{resp}</li>
+            })
+        }
+        return false
+    }
+
     return(
         <div>
             <h1>Questao</h1>
+            <div>
+                <span>{questao?.enunciado}</span>
+                <ul>
+                    {renderizarRespostas()}
+                </ul>
+            </div>
         </div>
     )
 }
