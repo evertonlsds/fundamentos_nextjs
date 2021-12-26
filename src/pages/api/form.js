a93dcb8a6658404e77946116e714818bcf1755fd
Everton santos <everton0102@gmail.com>
2021-12-25 21:24:55 -0300

    validando api form

--- src/pages/api/form.js
+++ src/pages/api/form.js
@@ -4,8 +4,10 @@ const usuarios = []
 export default function form(req, res){
    if(req.method === "POST"){
        post(req, res)
-    }else{
+    }else if(req.method === "GET"){
        get(req, res) 
+    }else{
+        res.status(405).send()
     }
    
 }
